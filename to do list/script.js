bb7e3728365f113588e2acac6022dd84e8fb4f8c
Ayush <120111007+ayushcoder11@users.noreply.github.com>
2023-09-19 01:29:41 +0530

    Add files via upload

---
+++ to do list/script.js
@@ -0,0 +1,17 @@
+let inputs = document.getElementById("inps");
+let text = document.querySelector(".text");
+function Add(){
+    if(inputs.value == ""){
+        alert("No Task Entered")
+    }
+    else{
+        let newElement = document.createElement("ul");
+        newElement.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newElement);
+        inputs.value="";
+        newElement.querySelector("i").addEventListener("click" , remove);
+        function remove(){
+            newElement.remove()
+        }
+    }
+}
